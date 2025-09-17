51fc2187ce960604c99455e3b03aff6e5fe05b55
SaiSindhuNov19 <saiusr25@gmail.com>
2025-09-17 12:38:52 +0530

    vite config file changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: './', // Important for Netlify
+  build: {
+    outDir: 'dist'
+  }
 })
